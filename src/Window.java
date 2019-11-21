f33d76f1527deec1858bb0ca87d9f91dca1b7df1
Puttipat Ketpupong <dee69232@gmail.com>
2019-11-21 12:06:09 +0700

    fix javafx

--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,11 @@
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
 
-public class Window {
-
+public class Window extends Canvas {
+	
+	public Window(int width,int height,String title) {
+		Group root=new Group();
+		Scene s=new Scene(root,width,height,)
+	}
+	
 }
