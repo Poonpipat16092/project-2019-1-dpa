ee06709062b82ca70ffcf2f451ceaabc730db109
Puttipat Ketpupong <dee69232@gmail.com>
2019-11-21 14:55:25 +0700

    add Application main

---
+++ src/Application/main.java
@@ -0,0 +1,17 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+	
+	public void start(Stage primaryStage) {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
