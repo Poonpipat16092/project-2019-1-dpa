14d1317cadade3fb4244ad976d035afaf153eed1
p.kamnerdtanmanee@gmail.com <54212937+Poonpipat16092@users.noreply.github.com>
2019-11-21 16:04:37 +0700

    add constructor

--- src/Graphic/StartScreen.java
+++ src/Graphic/StartScreen.java
@@ -1,11 +1,20 @@
 package Graphic;
 
+import javafx.stage.Stage;
+
 public class StartScreen implements Screen{
-	
+	private Stage primarystage ;
+	public public StartScreen(stage primarystage) {
+		// TODO Auto-generated constructor stub
+		this.primarystage = primarystage ;
+		
+		
+	}
+	@Override
 	public void draw() {
 		
 	}
-	
+	@Override
 	public void startanimation() {
 		
 	}
