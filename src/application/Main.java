e0af4801b097b5eb2d6222f138740c4ae6b9dc57
p.kamnerdtanmanee@gmail.com <54212937+Poonpipat16092@users.noreply.github.com>
2019-12-15 00:05:11 +0700

    change to default

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,12 +15,12 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primarystage) throws Exception {
-//		StartScreen startScreen = new StartScreen(primarystage);
-//		startScreen.startAnimation();
+		StartScreen startScreen = new StartScreen(primarystage);
+		startScreen.startAnimation();
 //		GameOverScreen end = new GameOverScreen(primarystage);
 //		end.startAnimation();
-		SelectPlayerScreen selectedPlayer = new SelectPlayerScreen(primarystage);
-		selectedPlayer.startAnimation();
+//		SelectPlayerScreen selectedPlayer = new SelectPlayerScreen(primarystage);
+//		selectedPlayer.startAnimation();
 		primarystage.show();
 	}
 
