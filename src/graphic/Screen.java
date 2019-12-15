73719392490ca54507783d34593c0398809eac0f
Puttipat Ketpupong <dee69232@gmail.com>
2019-12-15 11:27:37 +0700

    add spawner

--- src/graphic/Screen.java
+++ src/graphic/Screen.java
@@ -3,6 +3,7 @@ package graphic;
 import javafx.scene.canvas.GraphicsContext;
 
 public interface Screen {
+	
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600 ; 
 	public abstract void draw(GraphicsContext gc);
