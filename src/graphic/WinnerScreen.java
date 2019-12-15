35c6c6dbefaad27d6cc5cccecf6eca7c86d8cb7f
p.kamnerdtanmanee@gmail.com <54212937+Poonpipat16092@users.noreply.github.com>
2019-12-15 23:16:37 +0700

    build winner screen

---
+++ src/graphic/WinnerScreen.java
@@ -0,0 +1,19 @@
+package graphic;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class WinnerScreen implements Screen {
+
+	@Override
+	public void draw(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void startAnimation() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
