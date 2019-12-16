621aeee084bbc516db5b03098e4ac71538e48e97
p.kamnerdtanmanee@gmail.com <54212937+Poonpipat16092@users.noreply.github.com>
2019-12-16 15:44:45 +0700

    build Audiolaoder

---
+++ src/graphic/AudioLoader.java
@@ -0,0 +1,14 @@
+package graphic;
+
+import javafx.scene.media.AudioClip;
+
+public class AudioLoader {
+	
+	public static final AudioClip START_SCREEN_SOUND = new AudioClip(ClassLoader.getSystemResource("startScreenSound.mp3").toString());
+	public static final AudioClip MEGA_LASER = new AudioClip(ClassLoader.getSystemResource("megaLaser.wav").toString());
+	public static final AudioClip BUTTON_CLICK = new AudioClip(ClassLoader.getSystemResource("buttonclick.mp3").toString());
+	public static final AudioClip COLLET_SUPPLY = new AudioClip(ClassLoader.getSystemResource("collectsupply.mp3").toString());
+	public static final AudioClip DIED = new AudioClip(ClassLoader.getSystemResource("died.mp3").toString());
+	public static final AudioClip ERRORR = new AudioClip(ClassLoader.getSystemResource("error.mp3").toString());
+	
+}
